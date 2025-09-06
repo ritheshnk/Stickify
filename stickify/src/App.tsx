d7709cb2edcc27eb33a4e611ff35eb4c7922d2b5
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2025-09-06 19:30:51 +0530

    first commit

---
+++ stickify/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './index.css'
+import Header from './componets/Header'
+
+
+function App() {
+  return(
+    <>
+      <div className="">
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
