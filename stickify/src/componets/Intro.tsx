262625ca9bf04038b1bc070a30f2ff43b6660a25
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2025-09-16 23:56:35 +0530

    reduced the size of intro

--- stickify/src/componets/Intro.tsx
+++ stickify/src/componets/Intro.tsx
@@ -7,7 +7,7 @@ export default function Intro({title}: Props ) {
     return (
         <>
             <div className="flex flex-col items-center jutify-center space-y-4">
-               <h1 className="text-4xl sm:text-6xl font-bold h-40 flex items-center">
+               <h1 className="text-4xl sm:text-5xl font-bold h-40 flex items-center">
                   <span className="hover:text-red-500 transition-colors duration-200">{title}</span> 
                   &nbsp;It’s vibe certified.
                 </h1>
