be10e090ba5ab8b6c1efc9d69ccd0cbd75ed8256
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2025-10-01 01:01:03 +0530

    Update Animation.tsx

--- stickify/src/componets/Animation.tsx
+++ stickify/src/componets/Animation.tsx
@@ -2,7 +2,7 @@ export default function Animation() {
     const randomNum = Math.random();
     return(
         <>
-            
+            {/* dynamic class name */}
             <h1 className={`${randomNum>0.5?"text-red-500":"text-white-500"} flex flex-col text-center`}>This is Animation page</h1>
         </>
     )
